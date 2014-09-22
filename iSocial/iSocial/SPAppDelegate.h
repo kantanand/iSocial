15d0442af6f1bdc07a75c8843204e08788d958c2
Kantanand US <kantanand@users.noreply.github.com>
2014-09-23 01:14:48 +0530

    iSocial Scratch app

---
+++ iSocial/iSocial/SPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPAppDelegate.h
+//  iSocial
+//
+//  Created by kantanand us on 23/09/14.
+//  Copyright (c) 2014 mobisir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iSocial/iSocial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iSocial
+//
+//  Created by kantanand us on 23/09/14.
+//  Copyright (c) 2014 mobisir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPAppDelegate class]));
+    }
+}
