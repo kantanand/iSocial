70ec15320cd3f4654d617f8c6ff8bb2ab66db5f0
Kantanand US <kantanand@users.noreply.github.com>
2014-09-23 10:20:31 +0530

    new SPLoginPhoneView created for login view

---
+++ iSocial/PhoneViewController/SPLoginPhoneViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPLoginPhoneViewController.h
+//  iSocial
+//
+//  Created by kantanand us on 23/09/14.
+//  Copyright (c) 2014 mobisir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPLoginPhoneViewController : UIViewController
+
+@end
---
+++ iSocial/PhoneViewController/SPLoginPhoneViewController.m
@@ -0,0 +1,38 @@
+//
+//  SPLoginPhoneViewController.m
+//  iSocial
+//
+//  Created by kantanand us on 23/09/14.
+//  Copyright (c) 2014 mobisir. All rights reserved.
+//
+
+#import "SPLoginPhoneViewController.h"
+
+@interface SPLoginPhoneViewController ()
+
+@end
+
+@implementation SPLoginPhoneViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
